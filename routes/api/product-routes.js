544473cba2f622e9ad044233eb96ce33f30e0004
Jacob Jeffries <jeffrij85@gmail.com>
2023-03-12 15:07:20 -0400

    Product Delete route added

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -103,8 +103,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const delProduct = await Product.destroy( {where:{ id: req.params.id}});
+    res.status(200).json(delProduct);
+  } catch(err){
+    res.status(400).json(err);
+  };
 });
 
 module.exports = router;
